7ee23fa1bbe9ec5c12c35cccff94fd73f2f21a96
shinseongsu <tlstjdtn321@naver.com>
2026-06-21 00:21:09 +0900

    Revert "feat: replace JEJU GO lottie with ieum branded CSS animation"
    This reverts commit 3255dc3477c503705be441fc063fe2ef1dcb30fd.

--- app/(landing)/_components/landing-lottie.tsx
+++ app/(landing)/_components/landing-lottie.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import LandingAnimation from "./landing-animation";
+import Lottie from "react-lottie";
+import animationData from "@/public/lottie/landing.json";
 import LandingFooter from "./landing-footer";
 
 export default function LandingLottie() {
   return (
     <div className="relative bg-[#e8e8e8]">
-      <LandingAnimation />
+      <Lottie options={{ animationData, loop: true }} height="100dvh" />
       <div className="absolute bottom-0 w-full">
         <LandingFooter />
       </div>
